d500342724263aef929aa26c856c2d89a8954432
Abo7mood <74455601+Abo7mood@users.noreply.github.com>
2024-12-08 00:03:22 +0300

    project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Constructer
+   public static PlayerController instance;
+    Animator anim;
+    Transform cam;
+    CharacterController Character;
+    public GroundCheck groundcheck;
+
+    #endregion
+    #region float&int
+    private float horizontal;
+    private float vertical;
+    public float sprint;
+    public float _speed;
+    public float _verticalVelocity;
+    public float _gravity;
+    public float jumpHeight;
+    #endregion
+
+    #region boolean
+    private bool isSprint;
+    #endregion
+    #region Other
+    private Vector3 move;
+    private Vector3 Velocity;
+
+    #endregion
+    private void Awake()
+    {
+        instance = this;
+        Character = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+        cam = Camera.main.transform;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        JumpVoid();
+    }
+    void PlayerMove()
+    {
+        move = new Vector3(horizontal, 0, vertical);
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+
+        isSprint = Input.GetButton("Sprint");
+        sprint = isSprint ? 3f : 1;
+        anim.SetFloat("Speed", Mathf.Clamp(move.magnitude, 0, 0.5f) + (isSprint ? .5f : 0));
+
+        if (move.magnitude > 0.1f)
+        {
+            float angle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+
+       
+        move = cam.TransformDirection(move);
+
+
+        move = new Vector3(move.x * _speed * sprint, _verticalVelocity,
+      move.z * _speed * sprint);
+        Character.Move(move * Time.deltaTime);
+        Character.Move(Velocity * Time.deltaTime);
+
+        if (move.magnitude > 1)
+            move = move.normalized;
+     
+    }
+    private void JumpVoid()
+    {
+        //check if the player on the ground or not
+        if (groundcheck.isGrounded)
+        {
+            //jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                Velocity.y = jumpHeight;
+                anim.SetBool("IsJumping", true);
+            }
+
+        }
+        //fall
+        else
+        {
+            Velocity.y -= _gravity * Time.deltaTime;
+            anim.SetBool("IsJumping", false);
+        }
+
+    }
+}
---
+++ Assets/Scripts/SmoothMouseLook.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[AddComponentMenu("Camera-Control/Smooth Mouse Look")]
+public class SmoothMouseLook : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float fastMovementSpeed = 100f;
+    public float turnSpeed = 4.0f;
+    public float moveSpeed = 2.0f;
+    public float minTurnAngle = -90.0f;
+    public float maxTurnAngle = 90.0f;
+    private float rotX;
+    void Update()
+    {
+        MouseAiming();
+        KeyboardMovement();
+    }
+    void MouseAiming()
+    {
+        // get the mouse inputs
+        float y = Input.GetAxis("Mouse X") * turnSpeed;
+        rotX += Input.GetAxis("Mouse Y") * turnSpeed;
+        // clamp the vertical rotation
+        rotX = Mathf.Clamp(rotX, minTurnAngle, maxTurnAngle);
+        // rotate the camera
+        transform.eulerAngles = new Vector3(-rotX, transform.eulerAngles.y + y, 0);
+    }
+    void KeyboardMovement()
+    {
+        var fastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        var movementSpeed = fastMode ? this.fastMovementSpeed : this.movementSpeed;
+
+        Vector3 dir = new Vector3(0, 0, 0);
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+
+
+        transform.Translate(dir * movementSpeed * Time.deltaTime);
+    }
+}
